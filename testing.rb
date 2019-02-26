06263817db355781989355c03fc4766fc3ee8956
Kitchenerin <lenyajoker66@gmail.com>
2019-02-26 16:40:44 +0300

    orm

--- testing.rb
+++ testing.rb
@@ -3,6 +3,7 @@ require_relative 'cars'
 require_relative 'base_classes'
 require_relative 'operation'
 
+
 con = Users.open_connection
 id = gets.chomp
 name = gets.chomp
@@ -10,6 +11,8 @@ family_name = gets.chomp
 phone_number = gets.chomp
 Base.add( id: id, name: name, family_name: family_name, phone_number: phone_number)
 Users.save(id: id, name: name, family_name: family_name, phone_number: phone_number)
+
+
 id = gets.chomp
 name = gets.chomp
 family_name = gets.chomp
@@ -17,6 +20,8 @@ phone_number = gets.chomp
 Base.update(id: id, name: name,family_name: family_name, phone_number:phone_number)
 Users.show_results
 Users.close(con)
+
+
 id = gets.chomp
 name = gets.chomp
 family_name = gets.chomp
@@ -24,6 +29,9 @@ phone_number = gets.chomp
 user.where(con, id: id, name: name, family_name: family_name, phone_number: phone_number)
 Users.show_results
 Users.close(con)
+
+
+
 id = gets.chomp
 Base.destroy(id: id)
 Users.show_results
