b14ec5ec3323cf880ff42a53de36a6f0549c868f
Kitchenerin <lenyajoker66@gmail.com>
2019-02-28 12:26:18 +0300

    Ready ORM

--- base_classes.rb
+++ base_classes.rb
@@ -1,81 +1,82 @@
 require 'pg'
 
 class Bases
-	def initialize(find = 'names', user = 'family_name', query = 'add' )
-		@find = find
-		@user = user
-		@query = query
-	end
-	rowId = ARGV[0]
+  def initialize(find = 'names', user = 'family_name', query = 'add')
+    @find = find
+    @user = user
+    @query = query
+  end
 
-	DB_NAME = 'Byers'
-	DB_USER = 'user'
-	@table_name = 'byers'
+  rowId = ARGV[0]
 
-	class << self
-		def connection
-			PG.connect :dbname => DB_NAME, :user => DB_USER
-		end
+  DB_NAME = 'Byers'
+  DB_USER = 'user'
+  @table_name = 'byers'
 
-		def add(**options)
-			@query = "INSERT INTO #{@table_name} VALUES("
-			options.each { |k, v|
-				@query = @query << "'#{v}'" <<',' }
-			@query = @query.chomp(",")
-			connection.exec "#{@query<<')'}"
-			close_connection
-		end
+  class << self
+    def connection
+      PG.connect :dbname => DB_NAME, :user => DB_USER
+    end
 
-		def save (id, name, family_name, phone_number)
-			@base = Bases.new(id: id, name: name, family_name: family_name, phone_number: phone_number)
-			@base.save
-		end
+    def add(**options)
+      @query = "INSERT INTO #{@table_name} VALUES("
+      options.each { |k, v|
+        @query = @query << "'#{v}'" <<',' }
+      @query = @query.chomp(",")
+      connection.exec "#{@query<<')'}"
+      close_connection
+    end
 
-		def select(logic_operation=nil, **options)
-			#@query = "SELECT * FROM #{@table_name} WHERE"
-			names = ["SELECT * FROM #{@table_name} WHERE"]
-			options.each { |k, v|
-				@query = @query << " #{k}" << " = " << "'#{v}'" << " #{logic_operation}" }
-			#@query = query.chomp(logic_operation)
-			connection.exec "#{@query}"
-			close_connection
-		end
+    def save (id, name, family_name, phone_number)
+      @base = Bases.new(id: id, name: name, family_name: family_name, phone_number: phone_number)
+      @base.save
+    end
 
-		def update(name, **options)
-			@query = "UPDATE #{@table_name} SET #{@changedName}= "
-			rs = connection.exec "SELECT ARRAY(SELECT name FROM #{@table_name} LIMIT 3)"
-			rs.each do |row|
-				puts row
-			end
-			@changedName = rs[1]
-			options.each { |k, v| @query = @query << " #{k}" << " = " << "'#{v}'" }
-			@query = @query.chomp(' , ')
-			@query = @query << "WHERE "<< "id = '#{id}'"
-			connection.exec "#{@query}"
-			close_connection
-		end
+    def select(logic_operation=nil, **options)
+      #@query = "SELECT * FROM #{@table_name} WHERE"
+      names = ["SELECT * FROM #{@table_name} WHERE"]
+      options.each { |k, v|
+        @query = @query << " #{k}" << " = " << "'#{v}'" << " #{logic_operation}" }
+      #@query = query.chomp(logic_operation)
+      connection.exec "#{@query}"
+      close_connection
+    end
 
-		def destroy(**options)
-			query = "DELETE FROM #{@table_name} WHERE"
-			options.each { |k, v|
-				query = query << " #{k}" << " = " << "'#{v}'" }
-			response = connection.exec "#{query}"
-			close_connection
-		end
+    def update(name, **options)
+      @query = "UPDATE #{@table_name} SET #{@changedName}= "
+      rs = connection.exec "SELECT ARRAY(SELECT name FROM #{@table_name} LIMIT 3)"
+      rs.each do |row|
+        puts row
+      end
+      @changedName = rs[1]
+      options.each { |k, v| @query = @query << " #{k}" << " = " << "'#{v}'" }
+      @query = @query.chomp(' , ')
+      @query = @query << "WHERE "<< "id = '#{id}'"
+      connection.exec "#{@query}"
+      close_connection
+    end
 
-		def close_connection
-			connection.close
-		end
+    def destroy(**options)
+      query = "DELETE FROM #{@table_name} WHERE"
+      options.each { |k, v|
+        query = query << " #{k}" << " = " << "'#{v}'" }
+      response = connection.exec "#{query}"
+      close_connection
+    end
 
-		def table_name
-			self.name << 's'
-		end
+    def close_connection
+      connection.close
+    end
 
-		def show
-			connection.exec.each do |row|
-				puts "%s %s %s" % [row['id'], row['name'], row['family_name'], row['phone_number']]
-			end
+    def table_name
+      self.name << 's'
+    end
 
-		end
-	end
+    def show
+      connection.exec.each do |row|
+        puts "%s %s %s" % [row['id'], row['name'], row['family_name'], row['phone_number']]
+      end
+
+    end
+  end
 end
--- cars.rb
+++ cars.rb
@@ -2,7 +2,7 @@ require_relative 'base_classes'
 
 class Car < Bases
   def initialize(find = 'name', user = 'username', query = 'query')
-  super(find,user,query)
-    @names  = names
+    super(find, user, query)
+    @names = names
   end
 end
